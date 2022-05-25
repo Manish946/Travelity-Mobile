f8c4ea5ffd874c2b5e21b9b50bb86e3c481dae09
Manish946 <50454163+Manish946@users.noreply.github.com>
2022-05-25 13:39:35 +0200

    Phone Portrait only mode.

--- Travelity.Android/MainActivity.cs
+++ Travelity.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Acr.UserDialogs;
 
 namespace Travelity.Droid
 {
-    [Activity(Label = "Travelity", Icon = "@mipmap/Icon_Travelity", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Travelity", Icon = "@mipmap/Icon_Travelity", Theme = "@style/MainTheme", MainLauncher = true,ScreenOrientation =ScreenOrientation.Portrait ,ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
